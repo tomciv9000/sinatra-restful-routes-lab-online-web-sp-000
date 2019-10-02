bbe8501ccbc12d3a327f2c538fafc27504f17b4e
Tom White <eugene.white@gmail.com>
2019-10-02 02:42:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/recipes" do
+  get '/recipes' do
     erb :index
   end
   
-  get "/recipes/new" do
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  post '/recipes' do
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cooktime: params[:cooktime])
+    
+    redirect to "/articles/#{article.id}"
+  end
+  
+  get '/recipes' do
+    @articles = Article.all
     erb :index
   end
   
-  post "/recipes" do
-    erb :index
+  get '/recipes/:id' do
+    @article = Article.find(params[:id])
+    erb :show
   end
   
-  get "/recipes/:id" do
-    erb :index
+  get '/recipes/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+ 
+  patch '/recipes/:id' do 
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/recipes/#{@article.id}"
+  end
+  
+  delete '/recipes/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/recipes'
   end
   
   
